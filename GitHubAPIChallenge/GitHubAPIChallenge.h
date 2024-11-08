b34923fa788fb3a9e10af776cbeb988e8db7c711
Giovanni <giosvro@gmail.com>
2024-11-07 21:39:51 -0300

    Initial Commit

---
+++ GitHubAPIChallenge/GitHubAPIChallenge.h
@@ -0,0 +1,18 @@
+//
+//  GitHubAPIChallenge.h
+//  GitHubAPIChallenge
+//
+//  Created by Giovanni Severo on 07/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GitHubAPIChallenge.
+FOUNDATION_EXPORT double GitHubAPIChallengeVersionNumber;
+
+//! Project version string for GitHubAPIChallenge.
+FOUNDATION_EXPORT const unsigned char GitHubAPIChallengeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitHubAPIChallenge/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# GitHubAPIChallenge
+An Objective-C framework that encapsulates the GitHub REST API, providing a streamlined interface for interacting with GitHub resources.
