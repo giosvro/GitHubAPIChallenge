90ffeb341f8e81109b31ff15a4b43deb46b06c78
Giovanni <giosvro@gmail.com>
2024-11-13 21:01:51 -0300

    chore: add init methods to NetworkManager interface

--- GitHubAPIChallenge/NetworkManager/NetworkManager.h
+++ GitHubAPIChallenge/NetworkManager/NetworkManager.h
@@ -14,6 +14,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, strong) NSURLSession *session;
 
+- (instancetype)initWithSession:(NSURLSession *)session;
+
+- (instancetype)init;
+
 - (void)performRequestWithConfig:(id<RequestTypeProtocol>)config
                       completion:(void(^)(id _Nullable parsedObject, NSError * _Nullable error))completion;
 
