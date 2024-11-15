2a51cc0b40f8465c73b379edea4eedf3cfbe7ff9
Giovanni <giosvro@gmail.com>
2024-11-14 21:50:17 -0300

    chore: add documentation in the service classes

--- GitHubAPIChallenge/Services/Repository/List/RepositoryListService.h
+++ GitHubAPIChallenge/Services/Repository/List/RepositoryListService.h
@@ -10,13 +10,31 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * The `RepositoryListService` class provides functionality to fetch a list of repositories associated with a specific GitHub username.
+ * This service interacts with the GitHub API to retrieve repository information based on the specified username.
+ */
 @interface RepositoryListService : NSObject
 
+/**
+ * Initializes a new instance of `RepositoryListService`.
+ *
+ * @return A new instance of `RepositoryListService`.
+ */
 - (instancetype)init;
 
+/**
+ * Fetches a list of repositories for a specified GitHub username.
+ *
+ * @param username The GitHub username for which to fetch the repositories.
+ * @param completion A completion handler that is called when the request completes.
+ *        - If successful, `repositories` contains an array of `Repository` objects representing the user’s repositories, and `error` is `nil`.
+ *        - If an error occurs, `repositories` is `nil`, and `error` contains the error information.
+ */
 - (void)fetchRepositoriesForUsername:(NSString *)username
                           completion:(void(^)(NSArray<Repository *> * _Nullable repositories, NSError * _Nullable error))completion;
 
 @end
 
 NS_ASSUME_NONNULL_END
+
--- GitHubAPIChallenge/Services/Repository/Tags/RepositoryTagsService.h
+++ GitHubAPIChallenge/Services/Repository/Tags/RepositoryTagsService.h
@@ -10,10 +10,28 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * The `RepositoryTagsService` class provides functionality to fetch tags associated with a specific GitHub repository.
+ * This service interacts with the GitHub API to retrieve tag information based on the specified username and repository name.
+ */
 @interface RepositoryTagsService : NSObject
 
+/**
+ * Initializes a new instance of `RepositoryTagsService`.
+ *
+ * @return A new instance of `RepositoryTagsService`.
+ */
 - (nonnull instancetype) init;
 
+/**
+ * Fetches the list of tags for a specified GitHub repository.
+ *
+ * @param username The GitHub username that owns the repository.
+ * @param repo The name of the repository for which tags are to be fetched.
+ * @param completion A completion handler that is called when the request completes.
+ *        - If successful, `tags` contains an array of `Tag` objects representing the repository tags, and `error` is `nil`.
+ *        - If an error occurs, `tags` is `nil`, and `error` contains the error information.
+ */
 - (void)fetchRepositoryTagsWith:(NSString *)username
                            repo:(NSString *)repo
                      completion:(void(^)(NSArray<Tag *> * _Nullable tags, NSError * _Nullable error))completion;
